16f1ce2039f047291186712b27d865c75d5817c2
getsierralta <sierraltamaximiliano8@gmail.com>
2021-03-02 23:09:45 +0000

    Dao

--- src/main/java/com/security/demo/auth/ApplicationUser.java
+++ src/main/java/com/security/demo/auth/ApplicationUser.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 
 public class ApplicationUser implements UserDetails {
